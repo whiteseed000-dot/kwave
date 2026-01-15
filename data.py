e17aae0ea043fc41fd628e80ddf71a9efc59c9ec
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-15 23:38:54 +0800

    Update data.py

--- data.py
+++ data.py
@@ -3,8 +3,5 @@ import yfinance as yf
 def load_twii_monthly():
     df = yf.download("^TWII", start="1965-01-01", progress=False)
     df = df[['Close']].dropna()
-
-    # 🔑 轉成月資料
     df = df.resample("M").last()
-
     return df
