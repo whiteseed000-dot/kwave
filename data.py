7879bdf89f561158f2f4c5cfacdc1b370a7bb3d5
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-15 23:30:20 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 import yfinance as yf
+import pandas as pd
 
-
-df = yf.download('^TWII', start='1965-01-01')
+def load_twii():
+    df = yf.download("^TWII", start="1965-01-01", progress=False)
+    df = df[['Close']].dropna()
+    df.index = pd.to_datetime(df.index)
+    return df
